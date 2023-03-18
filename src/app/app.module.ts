c08cdcd1369e27ff397ec87288f87a608c348b5a
kannanATexcelforte <kannan@excelforte.com>
2023-03-18 20:07:39 +0530

    18.03.2023
    154. create basic ui has completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatButtonModule } from '@angular/material/button';
+import { MaterialModule } from './material/material.module';
+import { BookingComponent } from './components/booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
