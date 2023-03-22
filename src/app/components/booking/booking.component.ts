028d4d0699762eb364f92a6e87ceb0a015e712ed
kannanATexcelforte <kannan@excelforte.com>
2023-03-22 09:09:04 +0530

    22.03.2023
    162. autocomplete has completed

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -14,12 +14,35 @@ export class BookingComponent implements OnInit {
   countries: any;
   formGroup: FormGroup | any;
   customErrorStateMatcher: CustomErrorStateMatcher = new CustomErrorStateMatcher();
+  cities: any[] = [
+    { id: 1, cityName: "Abu Dhabi" },
+    { id: 2, cityName: "Amsterdam" },
+    { id: 3, cityName: "Berlin" },
+    { id: 4, cityName: "Chicago" },
+    { id: 5, cityName: "Doha" },
+    { id: 6, cityName: "Dubai" },
+    { id: 7, cityName: "Istanbul" },
+    { id: 8, cityName: "Las Vegas" },
+    { id: 9, cityName: "London" },
+    { id: 10, cityName: "Los Angeles" },
+    { id: 11, cityName: "Moscow" },
+    { id: 12, cityName: "New York" },
+    { id: 13, cityName: "Paris" },
+    { id: 14, cityName: "San Francisco" },
+    { id: 15, cityName: "Seoul" },
+    { id: 16, cityName: "Singapore" },
+    { id: 17, cityName: "Sydney" },
+    { id: 18, cityName: "Tokyo" },
+    { id: 19, cityName: "Toronto" },
+    { id: 20, cityName: "Washington" }
+  ];
 
   constructor(private countriesService: CountriesService) {
     this.formGroup = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       customerName: new FormControl(null, [Validators.required, Validators.maxLength(30), Validators.pattern('^[A-Za-z. ]*$')]),
       country: new FormControl(null, Validators.required),
+      city: new FormControl(null)
     });
   }
 
@@ -34,7 +57,7 @@ export class BookingComponent implements OnInit {
   }
 
   // returns the form control instance based on the control name
-  getFormControl(controlName: string):FormControl {
+  getFormControl(controlName: string): FormControl {
     return this.formGroup.get(controlName) as FormControl;
   }
 
@@ -42,29 +65,29 @@ export class BookingComponent implements OnInit {
   getErrorMessage(controlName: string, errorType: string): string {
     // controlName = customerName
     // errorType = 'required'
-    switch(controlName) {
+    switch (controlName) {
       case "customerName": {
-        if(errorType === 'required') {
+        if (errorType === 'required') {
           return 'You must specify <strong>Name</strong>';
-        } else if(errorType === 'maxlength') {
+        } else if (errorType === 'maxlength') {
           return '<strong>Name</strong> can contain upto 30 characters only';
-        } else if(errorType === 'pattern') {
+        } else if (errorType === 'pattern') {
           return '<strong>Name</strong> can contain alphabets or dot(.) or space only';
         } else {
           return '';
         }
       }
       case "email": {
-        if(errorType === 'required') {
+        if (errorType === 'required') {
           return '<strong>Email</strong> can\'t be blank';
-        } else if(errorType === 'email') {
+        } else if (errorType === 'email') {
           return '<strong>Email</strong> should be in correct format: Eg: someone@example.com';
         } else {
           return '';
-        }        
+        }
       }
       case 'country': {
-        if(errorType === 'required') {
+        if (errorType === 'required') {
           return 'You must choose a <strong>Country</strong>';
         } else {
           return '';
