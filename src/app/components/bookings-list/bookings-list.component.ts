97be9027b56ee38e1fd05916f3c5f859f7c8df07
kannanATexcelforte <kannan@excelforte.com>
2023-03-28 16:36:28 +0530

    28.03.2023
    182. table filter has completed

--- src/app/components/bookings-list/bookings-list.component.ts
+++ src/app/components/bookings-list/bookings-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -21,12 +22,17 @@ export class BookingsListComponent implements OnInit {
   isLoadingCompleted: boolean = false;
   bookingLoadingStatus: string = 'Loading...';
   isError: boolean = false;
+  formGroup: FormGroup | any;
 
-  constructor(private bookingsService: BookingsService) { }
+  constructor(private bookingsService: BookingsService) {
+    this.formGroup = new FormGroup({
+      search: new FormControl(null)
+    });
+  }
 
   ngOnInit(): void {
     this.bookingsService.getBookings().subscribe((response: Booking[]) => {
-      
+
       // create data source based on the response (Booking[])
       this.bookings = new MatTableDataSource<Booking>(response);
 
@@ -41,11 +47,37 @@ export class BookingsListComponent implements OnInit {
 
       // isLoadingCompleted is true
       this.isLoadingCompleted = true;
+
+      // filterPredicate
+      this.bookings.filterPredicate = (data, filter) => {
+        let data2 = { ...data }
+        if (filter) {
+          filter = filter.toLowerCase();
+        }
+        if (data2.customerName) {
+          data2.customerName = data2.customerName.toLowerCase();
+        }
+        if (data2.location) {
+          data2.location = data2.location.toLowerCase();
+        }
+        return data2.customerName.indexOf(filter) != -1 || data2.location.indexOf(filter) != -1;
+      }
     }, (error) => {
       console.log(error);
       this.bookingLoadingStatus = 'Error fetching the data';
       this.isError = true;
     });
+  }
+
+  filterBookings() {
+    if (this.formGroup.value.search != null && this.bookings) {
+      this.bookings.filter = this.formGroup.value.search.trim();
+    }
+  }
+
+  clearFilter() {
+    this.formGroup.patchValue({ search: '' });
+    this.filterBookings();
   }
 
 }
