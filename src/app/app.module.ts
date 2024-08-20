19e65901fa9b5b8c835e66271a79afe2367ea9cb
kannanATexcelforte <kannan@excelforte.com>
2024-08-20 10:30:16 +0530

    20.08.2024-ct
    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TableComponent } from './table/table.component';
-import { CatsListComponent } from './cats-list/cats-list.component';
-import { AutoCompleteComponent } from './auto-complete/auto-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TableComponent,
-    CatsListComponent,
-    AutoCompleteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
