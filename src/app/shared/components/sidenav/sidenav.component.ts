e8d139313252e7a124ca4b1cdd59b2c7a78d19ae
kannanATexcelforte <kannan@excelforte.com>
2024-08-21 15:40:54 +0530

    21.08.2024-st
    completed

---
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,27 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+  themeColor: 'primary' | 'accent' | 'warn' = 'primary'; // ? notice this
+  isDark = false; // ? notice this
+  constructor(private overlayContainer: OverlayContainer) { }
+
+  ngOnInit(): void { }
+
+  // ? notice below
+  toggleTheme(): void {
+    this.isDark = !this.isDark;
+    if (this.isDark) {
+      this.overlayContainer.getContainerElement().classList.add('dark-theme');
+    } else {
+      this.overlayContainer
+        .getContainerElement()
+        .classList.remove('dark-theme');
+    }
+  }
+}
